186e24651de5d2d4ec75e2d176f32cc8e7711de4
shatter9652 <nathanielswayze2003@gmail.com>
2026-03-23 12:54:58 -0400

    Update settings.py

--- app/settings.py
+++ app/settings.py
@@ -14,6 +14,7 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 from django.core.exceptions import ImproperlyConfigured
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
